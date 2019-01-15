a1a8274a8d11de6029e15da7f6a3526c10c1258f
Z3T4 Z00K <35911796+z3t4z00k@users.noreply.github.com>
2019-01-15 11:47:02 +0530

    Completed Question 1.1

--- LabAss3.cpp
+++ LabAss3.cpp
@@ -2,6 +2,12 @@
 #include <windows.h>
 using namespace std;
 
+/* Solution functions */
+void oneAns();
+//void twoAns();
+//void thrAns();
+
+
 /*    PROGRAM UTILS   */
 void error(){
   cout << endl << "Error encountered. restarting program..." << endl;
@@ -47,16 +53,79 @@ void manage(int n){
       break;
     case 2:
       ques(2);
-      twoAns();
+      //twoAns();
       break;
     case 3:
       ques(3);
-      thrAns();
+      //thrAns();
       break;
   }
   pause();
 }
 
+/*  ONE ANSWER  */
+struct interval{
+  int l, h;
+};
+
+struct node{
+  interval * i;
+  int max;
+  node* left;
+  node* right;
+};
+
+node* newNode(interval range){
+  node* temp = new node;
+  temp->i = new interval(range);
+  temp->max = range.h;
+  temp->left = temp->right = NULL;
+  return temp;
+}
+
+node* insert(node* root, interval range){
+  if(root == NULL)
+    return newNode(range);
+  int low = root->i->l;
+  if(range.l < root->max)
+    root->left = insert(root->left, range);
+  else
+    root->right = insert(root->right, range);
+  if(root->max < range.h)
+    root->max = range.h;
+  return root;
+}
+
+bool doOverlap(interval range1, interval range2){
+  if(range1.l <= range2.h && range2.l <= range1.h)
+    return true;
+  return false;
+}
+
+interval* overlap(node* root, interval range){
+  if(root == NULL){
+    return NULL;
+  }
+  if(doOverlap(*(root->i), range))
+    return root->i;
+  if(root->left != NULL && root->left->max >= range.l)
+    overlap(root->left, range);
+  return overlap(root->right, range);
+}
+
+void oneAns(){
+  interval ranges[] = {{0,9}, {10,19}, {20, 29}, {30, 39}, {40, 49}};
+  node* root = NULL;
+  int n = sizeof(ranges)/sizeof(ranges[0]);
+  for(int i = 0; i<n; i++)
+    root = insert(root, ranges[i]);
+  interval target={6,25};
+  interval *container = overlap(root, target);
+  cout << "Interval containing the target range {" << target.l << ", " << target.h << "} is : {" << container->l << ", " << container->h << "}." << endl;
+  return;
+}
+
+/*  DRIVER FUNCTION */
 int main(){
   while(1){
     system("cls");

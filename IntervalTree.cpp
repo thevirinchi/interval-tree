7a9bf1e7ce51bf3356504f4a2a581a8d4eca5096
Z3T4 Z00K <35911796+z3t4z00k@users.noreply.github.com>
2019-01-15 11:19:46 +0530

    working

--- IntervalTree.cpp
+++ IntervalTree.cpp
@@ -31,6 +31,23 @@ node* insert(node* root, interval range){
   return root;
 }
 
+bool doOverlap(interval range1, interval range2){
+  if(range1.low <= range2.high && range2.low <= range1.high)
+    return true;
+  return false;
+}
+
+interval* overlap(node* root, interval range){
+  if(root == NULL){
+    return NULL
+  }
+  if(doOverlap(*(root->i), range))
+    return root;
+  if(root->left != NULL && root->left->max >= range.low)
+    overlap(root->left, range);
+  return overlap(root->right, range);
+}
+
 int main(){
   return 0;
 }
